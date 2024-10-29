c8b3f002500b97784d2c6910fd9993673c873b17
Shantelle Su <ssubkhan@umich.edu>
2024-10-29 09:39:55 -0400

    last changes

--- purchase-form-main/js/form.js
+++ purchase-form-main/js/form.js
@@ -7,3 +7,18 @@ function sameAddress(button) {
 		document.querySelector("#bill-street").value = "";
 	}
 }
+
+function checkEmails(){
+    console.log("Changed the email")
+    let em1 = document.querySelector("#email")
+    let em2 = document.querySelector("#email2")
+    console.log(em1.value)
+    console.log(em2.value)
+
+    if (em1.value != em2.value){
+        alert("The two emails must match")
+        em2.focus()
+        return false;    
+    }
+    return true;
+}
